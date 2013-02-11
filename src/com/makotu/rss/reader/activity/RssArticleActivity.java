ebeae58f2048efcbce1ebbddb57794d129c848e7
MAKOTO SATO <fuiumi7@gmail.com>
2013-02-11 23:12:59 +0900

    add Progress Dialog when loading webpage.

--- src/com/makotu/rss/reader/activity/RssArticleActivity.java
+++ src/com/makotu/rss/reader/activity/RssArticleActivity.java
@@ -1,7 +1,9 @@
 package com.makotu.rss.reader.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -16,9 +18,31 @@ public class RssArticleActivity extends RssBaseActivity {
         //RSS‹LŽ–ˆê——‰æ–Ê‚©‚ç‚ÌIntent‚ðŽæ“¾
         Intent intent = getIntent();
         String url = intent.getStringExtra(INTENT_PUTEXTRA_KEY);
+        
+        final ProgressDialog loading = new ProgressDialog(this);
+        loading.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+
+        WebViewClient client = new WebViewClient() {
+        public void onPageFinished(final WebView view, final String url) {
+            if (loading.isShowing()) {
+                loading.dismiss();
+            }
+        };
+
+        public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+            loading.show();
+            };
+        };
+
+        WebChromeClient chrome = new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                loading.setProgress(progress);
+            }
+        };
 
         WebView webView = new WebView(this);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(client);
+        webView.setWebChromeClient(chrome);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl(url);
 
